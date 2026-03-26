a23963297db9dda71596ef0e739845656ecb29c8
Aila Gabriela <ailagabriela01@gmail.com>
2026-03-26 10:19:38 -0300

    feat: :sparkles: Lemos e tentamos fazer
    Começamos a fazer o site,

--- pokedex/app/welcome/welcome.tsx
+++ pokedex/app/welcome/welcome.tsx
@@ -4,9 +4,10 @@ export function Welcome() {
 //border 2px solid black, black
 
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <h1>Oi Bryan!</h1>
-      <button className="border-2 border-red-600 bg-red-600 hover:bg-red-400 bg-red-400 p-2" onClick={() => console.log("oi")}>aperte para falar Oi</button>
+    <main className="container-principal">
+      <div className="faixa-topo"></div>
+      <h1 className="conteudo">Pokédex</h1>
+      <button className="border-2 border-red-600">Home     Pokémons     Tipos</button>
     </main>
   );
 }
