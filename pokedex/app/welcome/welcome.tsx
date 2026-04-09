04d2c87b7a7881a7a3c0b45550b419127ee4e74e
Aila Gabriela <ailagabriela01@gmail.com>
2026-04-09 10:17:01 -0300

    feat: :sparkles: Melhorei o código
    melhorei a estrutura do código

--- pokedex/app/welcome/welcome.tsx
+++ pokedex/app/welcome/welcome.tsx
@@ -1,14 +1,27 @@
-
 export function Welcome() {
-
-//border 2px solid black, black
-
   return (
-    <main className="container-principal">
-      <div className="faixa-topo"></div>
-      <h1 className="conteudo">Pokédex</h1>
-      <button className="border-2 border-red-600">Home     Pokémons     Tipos</button>
+    <main className="min-h-screen flex flex-col justify-between bg-pink-100 ">
+      <nav className="bg-red-600 p-7 flex items-center justify-between shadow-md">
+        
+        <h1 className="text-white text-2xl font-bold">
+          Pokédex
+        </h1>
+
+        <div className="flex gap-4">
+          <button className="text-white hover:underline">Home</button>
+          <button className="text-white hover:underline">Pokémons</button>
+          <button className="text-white hover:underline">Tipos</button>
+        </div>
+
+      </nav>
+      
+      <div className="flex-grow">
+
+      </div>
+
+      <footer className="bg-red-300 p-4">
+        <p className="text-white text-center font-bold">Pokédex, React + Tailwind</p>
+      </footer>
     </main>
   );
 }
-
